f8c56dcee558a5cd555903cf30c63cfd88207956
Onandi Skeen <onandiskeen@gmail.com>
2022-04-04 23:34:37 -0400

    added news page

---
+++ src/views/NewsView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="news container">
+        <div class="text-container">
+            <h2>News</h2>
+            <NewsList />
+        </div>
+    </div>
+</template>
+
+<script>
+    import NewsList from '@/components/NewsList.vue'
+
+    export default {
+    data() {
+    return {};
+    },
+    components: { NewsList }
+    };
+</script>
