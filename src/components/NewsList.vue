efb1de32cbbdda94dd8d93e87f6bebd9ce76ded1
Onandi Skeen <onandiskeen@gmail.com>
2022-04-04 23:04:45 -0400

    Added news and search bar

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,4 +1,16 @@
 <template>
+    <form @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+ <div class="input-group mx-sm-3 mb-2">
+ <label class="visually-hidden" for="search">Search</label>
+ <input type="search" name="search" v-model="searchTerm"
+id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
+ search term here" />
+ <button class="btn btn-primary mb-2">Search</button>
+ </div>
+ <p>You are searching for {{ searchTerm }}</p>
+ </form>
+
+<div class="container_view">
     <div class="card" v-for="article in articles" style="width: 20rem; height: 30rem;">
         <img class="card-img-top" :src=" article.urlToImage " alt="News Image" style="height: 10rem">
 
@@ -7,6 +19,8 @@
             <p class="card-text">{{ article.description }}</p>
         </div>
     </div>
+</div>
+    
     
 </template>
 
@@ -14,9 +28,11 @@
 export default {
  data() {
  return {
- articles: []
+ articles: [],
+ searchTerm : ''
  }
  },
+ 
  created() {
      let self = this;
 
@@ -33,7 +49,26 @@ export default {
  console.log(data);
  self.articles = data.articles;
  });
+ },
+ methods: {
+ searchNews() {
+ let self = this;
+ console.log(self.searchTerm)
+ fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en', {
+ headers: {
+ 'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`,
  }
+})
+ .then(function(response) {
+ return response.json();
+ })
+ .then(function(data) {
+ console.log(data);
+ self.articles = data.articles;
+ });
+ }
+ }
+ 
 };
 </script>
 
@@ -41,4 +76,12 @@ export default {
     .card{
         margin: 10px;
     }
+
+      .container_view {
+    max-width: 100%;
+    flex-wrap: wrap;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,22 +16,12 @@ export default {
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
-        <div class="grid">
-          <div class="container_view">
             <NewsList />
-          </div>
         </div>
       </div>
-    </div>
 </template>
 
 <style>
-  .container_view {
-    max-width: 100%;
-    flex-wrap: wrap;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
+
 
 </style>
